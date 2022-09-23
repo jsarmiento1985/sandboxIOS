df816a208d622f2f6420636e66780ce7f77dca63
Furia55 <jsarmiento1985@hotmail.com>
2022-09-23 15:31:09 -0500

    stacks

--- sandBox/ContentView.swift
+++ sandBox/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world! james sarmiento")
+        ZStack {
+    
+            VStack(alignment:.leading, spacing: 15) {
+                Text("Siguiendo")
+                    .font(.largeTitle)
+                    .bold()
+                Text("Canales recomendados").foregroundColor(.gray)
+                HStack {
+                    Rectangle()
+                        .foregroundColor(.blue)
+                    .frame(width:118, height:68)
+                    
+                    VStack (alignment: .leading) {
+                        HStack {
+                            Circle().frame(width:20, height: 20).foregroundColor(.red)
+                            Text("Username").bold().font(.headline)
+                        }
+                        Text("Streaming de programacion...").bold().foregroundColor(.gray)
+                        Text("Solo hablando").bold().foregroundColor(.gray)
+                    }
+                    
+                }
+          
+            }
+            Text("Suscribir a SwiftBeta")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.red)
+                .underline()
+                .background(Color.black)
+                .rotationEffect(.degrees(-20))
         }
-        .padding()
+       
     }
 }
 
