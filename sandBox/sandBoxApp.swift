68a426b8a294ba561b935035cdbf256a706741d0
Furia55 <jsarmiento1985@hotmail.com>
2022-09-23 12:07:19 -0500

    Initial Commit

---
+++ sandBox/sandBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandBoxApp.swift
+//  sandBox
+//
+//  Created by James Sarmiento on 23/09/22.
+//
+
+import SwiftUI
+
+@main
+struct sandBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
